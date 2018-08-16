b5ad82f58f108cb1fbbf1b69a963bec23c4b07da
Justin Evankovich <justinevankovich@gmail.com>
2018-08-16 17:04:30 -0400

    Forth program to do getc putc works now

--- forth.s
+++ forth.s
@@ -30,9 +30,9 @@ docolon:
         sub     rbp, 8
         mov     [rbp], rsi      ; push the current instruction pointer
 
-        add     rax, 8
-        mov     rsi, [rax]
-        jmp     [rsi]
+        mov     rsi, rax
+        add     rsi, 8          ; point rsi at the first sub-word
+        jmp     next
 
 doconst:
         push    rbx             ; push down the top-of-stack
@@ -44,54 +44,6 @@ dovar:
         mov     rbx, rax
         add     rbx, 8
         jmp     next
-
-;; Reads a single byte from standard in.
-;; The read byte is returned as a 16 bit number in ax.
-;; If the returned number is negative, EOF has been reached
-;getc:   mov     r8, [buf_in_ind]; r8 is the current index into the buffer
-;        cmp     r8, [buf_in_len]
-;        jne     getc_skip_read
-;          mov   rdi, 0          ; stdin
-;          mov   rsi, buf_in
-;          mov   rdx, 4096
-;          mov   rax, 0          ; read
-;          syscall
-;
-;          cmp   rax, 0
-;          jg    getc_eof
-;            mov ax, -1
-;            ret
-;getc_eof:
-;          mov   [buf_in_len], rax
-;          mov   r8, 0           ; reset the index
-;          mov qword [buf_in_ind], 0
-;getc_skip_read:
-;        xor     ax, ax
-;        mov     al, [buf_in + r8]
-;        inc     r8
-;        mov     [buf_in_ind], r8; store the index
-;        ret
-;
-;; Writes the character in dil to standard out.
-;putc:   mov     r8, [buf_out_len]
-;        mov     [buf_out + r8], dil
-;        inc     r8
-;        mov     [buf_out_len], r8
-;        ; If newline or we reach the end of the buffer, flush the buffer
-;        cmp     dil, 10
-;        je      putc_write
-;        cmp     r8, 4096
-;        jl      putc_write
-;        
-;        ret
-;putc_write:
-;        mov     rax, 1          ; write()
-;        mov     rdi, 1          ; stdout
-;        mov     rsi, buf_out
-;        mov     rdx, r8         ; [buf_out_len] is the number of bytes in the buffer
-;        syscall
-;        mov word [buf_out_len], 0
-;        ret
 
 ; The format of a dictionary entry is as follows
 ;       align 8
@@ -112,12 +64,12 @@ head    %1, %%name
 %endmacro
 
 %macro head 2
-        align   8
 %%link: dq      link
 %define link    %%link
 
 %strlen %%namelen %2
         db      %%namelen, %2
+
 x_ %+ %1:
 %endmacro
 
@@ -157,6 +109,41 @@ primitive add,'+'       ; ( a b -- a+b )
         add     rbx, rax
         jmp     next
 
+primitive or            ; ( a b -- a|b )
+        pop     rax
+        or      rbx, rax
+        jmp     next
+
+primitive eq,'='        ; ( a b -- bool )
+        pop     rax
+        cmp     rax, rbx
+        je      .eq
+        mov     rbx, 0
+        jmp     next
+.eq:    mov     rbx, -1
+        jmp     next
+
+primitive branch,'branch' ; ( -- ) increments the program counter by imm
+                          ; branch 0 is a nop
+        mov     rax, [rsi]
+        imul    rax, 8
+        add     rsi, rax        ; skip imm instructions
+        add     rsi, 8          ; skip one more because of the imm
+        jmp     next
+
+primitive cbranch,'?branch' ; ( bool -- ) increments the program counter by imm if top of stack is false
+                            ; ?branch 0 is a nop
+        pop     rax
+        test    rbx, rbx
+        jnz     .done
+        mov     rbx, [rsi]
+        imul    rbx, 8
+        add     rsi, rbx        ; skip imm instructions
+
+.done:  add     rsi, 8          ; skip a single instruction (e.g. skip the immediate)
+        mov     rbx, rax
+        jmp     next
+
 primitive sub,'-'       ; ( a b -- a-b )
         pop     rax
         sub     rax, rbx
@@ -189,18 +176,30 @@ primitive load,'@'      ; ( a -- [a] )
         mov     rbx, [rbx]
         jmp     next
 
-primitive store,'!'     ; ( a x -- ) stores x at a
+primitive loadb,'@b'    ; ( a -- [a] )
+        xor     rax, rax
+        mov     al, [rbx]       ; read the byte at rbx
+        mov     rbx, rax
+        jmp     next
+
+primitive store,'!'     ; ( x a -- ) stores x at a
         pop     rax
-        mov     [rax], rbx
+        mov     [rbx], rax
         pop     rbx
         jmp     next
 
-primitive exit
+primitive storeb,'!b'   ; ( b a -- ) stores b at a
+        pop     rax
+        mov     [rbx], al
+        pop     rbx
+        jmp     next
+
+primitive exit  ; ( x -- )
         mov     rax, 60         ; exit
         mov     rdi, rbx        ; exit code rbx
         syscall
 
-primitive read
+primitive read  ; ( fd buf count -- bytes-read )
         mov     r8, rsi         ; save rsi
         mov     rax, 0          ; read
         mov     rdx, rbx        ; count is top of stack
@@ -211,7 +210,7 @@ primitive read
         mov     rsi, r8         ; restore rsi
         jmp     next
 
-primitive write
+primitive write ; ( fd buf count -- bytes-read )
         mov     r8, rsi         ; save rsi
         mov     rax, 1          ; write
         mov     rdx, rbx        ; count is top of stack
@@ -222,21 +221,116 @@ primitive write
         mov     rsi, r8         ; restore rsi
         jmp     next
 
-colon main
-; 1 buf_in 0 buf_in 4096 read write exit
+colon incvar    ; ( v -- )
+; dup @ 1 + swap !
+        dq      x_dup
+        dq      x_load
         dq      x_imm,1
+        dq      x_add
+        dq      x_swap
+        dq      x_store
+        dq      x_done
+
+colon getc      ; ( -- c )
+; buf_in_len @ buf_in_ind @ = if
+;       stdin buf_in buf_in_size read buf_in_len ! 
+;       0 buf_in_ind !
+;       then
+; buf_in buf_in_ind @ + @b
+; buf_in_ind incvar
+        dq      x_buf_in_len
+        dq      x_load
+        dq      x_buf_in_ind
+        dq      x_load
+        dq      x_eq
+        dq      x_cbranch,9
+
+        dq      x_stdin
         dq      x_buf_in
-        dq      x_imm,0
-        dq      x_buf_in
-        dq      x_imm,4096
+        dq      x_buf_in_size
         dq      x_read
+        dq      x_buf_in_len
+        dq      x_store
+
+        dq      x_zero
+        dq      x_buf_in_ind
+        dq      x_store
+
+        dq      x_buf_in
+        dq      x_buf_in_ind
+        dq      x_load
+        dq      x_add
+        dq      x_loadb
+
+        dq      x_buf_in_ind
+        dq      x_incvar
+        
+        dq      x_done
+
+colon putc      ; ( c -- )
+; dup
+; buf_out buf_out_len @ + !b
+; buf_out_len incvar 
+; '\n' = buf_out_size buf_out_len @ = or if
+;       stdout buf_out buf_out_len @ write drop
+;       0 buf_out_len !
+;       then
+        dq      x_dup
+
+        dq      x_buf_out
+        dq      x_buf_out_len
+        dq      x_load
+        dq      x_add
+        dq      x_storeb
+
+        dq      x_buf_out_len
+        dq      x_incvar
+
+        dq      x_imm,10        ; \n
+        dq      x_eq
+        dq      x_buf_out_size
+        dq      x_buf_out_len
+        dq      x_load
+        dq      x_eq
+        dq      x_or
+        dq      x_cbranch,9
+
+        dq      x_stdout
+        dq      x_buf_out
+        dq      x_buf_out_len
+        dq      x_load
         dq      x_write
-        dq      x_exit
+        dq      x_drop
+
+        dq      x_zero
+        dq      x_buf_out_len
+        dq      x_store
+
+        dq      x_done
+
+colon main
+; getc putc branch -4
+        dq      x_getc,x_putc
+        dq      x_branch,-4
 
 section .data
 
+constant zero,'0'       ; convenience constant
+        dq      0
+
+constant stdin
+        dq      0
+
+constant stdout
+        dq      1
+
+%define buf_size 512
+
 constant buf_in
         dq      buf_in
+
+constant buf_in_size
+        dq      buf_size
 
 variable buf_in_len
         dq      0
@@ -246,6 +340,9 @@ variable buf_in_ind
 
 constant buf_out
         dq      buf_out
+
+constant buf_out_size
+        dq      buf_size
 
 variable buf_out_len
         dq      0
@@ -257,5 +354,5 @@ section .bss
         resq    1024
 r_stack:
 
-buf_in: resb    4096
-buf_out:resb    4096
+buf_in: resb    buf_size
+buf_out:resb    buf_size
